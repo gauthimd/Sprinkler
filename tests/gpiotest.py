d5a588315a82ba3ba4fd9f6035d379427715d77e
Mike Gauthier <gauthiermichael3.14@gmail.com>
2016-09-13 15:20:41 -0500

    More tests and such

--- tests/gpiotest.py
+++ tests/gpiotest.py
@@ -2,17 +2,22 @@
 #for the sprinkler station
 import RPi.GPIO as GPIO
 import time
-
-l = [5,6,13,19,26,12,16,20] #These are the BCM GPIO pin numbers
+now = time.time()
+l = [27,22,13,19,26,12,16,20] #These are the BCM GPIO pin numbers
+iter = 25 #number of iterations for the loop
 sec = .05 #Number of seconds each LED will be on
+estime = iter*8*sec
+print "Estimated runtime: ", estime, " secs"
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 for y in l:
  GPIO.setup(y, GPIO.OUT)
  GPIO.output(y, GPIO.LOW)
-for x in range(100):
+for x in range(iter):
  for y in l:
   GPIO.output(y, GPIO.HIGH)
   time.sleep(sec)
   GPIO.output(y, GPIO.LOW)
 GPIO.cleanup()
+done = time.time()
+print "Calculated runtime: ", round(done - now,1), " secs"
---
+++ tests/tuplestest.py
@@ -0,0 +1,8 @@
+l = [(1,20),(2,10),(3,15),(4,15)]
+for x in l:
+  y = x
+  n = y[0]
+  m = y[1]
+  print x
+  print n
+  print m
